bec9c53f0ae62cb3a844a48c95d687e032c79be7
Nishat <minhazur.cse.00724205101053@aust.edu>
2026-02-13 22:53:40 +0600

    Nishat

--- LevelDevil/Levelone.h
+++ LevelDevil/Levelone.h
@@ -8,8 +8,8 @@ int obstacleHeight = 60;
 
 int noOfObj = 2;
 
-GameObject obj1 = { 300, 60, 100, 20, 0, 1, true, false };
-GameObject obj2 = { 500, 60, 100, 20, 0, 1, true, true };
+GameObject obj1 = { 300, 60, 60, 20, 0, 1, true, false };
+GameObject obj2 = { 500, 60, 60, 20, 0, 1, true, true };
 
 GameObject obj[] = { obj1, obj2 };
 
